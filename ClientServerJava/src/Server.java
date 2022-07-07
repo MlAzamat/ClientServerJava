7aa23c041ea4d2156bec3e7d176dc9726359dd42
MlAzamat <33450014+MlAzamat@users.noreply.github.com>
2022-07-07 22:22:19 +0300

    Idea firstSimpleServer

--- ClientServerJava/src/Server.java
+++ ClientServerJava/src/Server.java
@@ -1,2 +1,28 @@
+import java.net.*;  //for work with network
+import java.io.*;   //for input output
+
+
 public class Server {
+
+    public static void main(String[] args) throws IOException {
+
+        ServerSocket serverSocket = new ServerSocket(8000); //Prepare port
+        int count = 0;
+        while (true) {
+            Socket clientSocket = serverSocket.accept();  //Get connection with Client  Open clientSocket  Listener to port
+            
+            System.out.println("Client accepted " + (count++));
+            OutputStreamWriter writer = new OutputStreamWriter(clientSocket.getOutputStream());
+            writer.write(               //Send text
+                    "HTTP/1.0 200 OK\r\n" +
+                            "Content-type: text/html\r\n" +
+                            "\r\n" +
+                            "<h1>Hello " + count + "/h1>\r\n");
+            writer.flush();     //Guarantees sending
+            writer.close();     //Close writer
+
+            clientSocket.close();   //Close clientSocket
+        }
+        //serverSocket.close();   //Close connection with Client
+    }
 }
